50bd596eea2b62381707c3be58eb2b32ec91d158
Laurie <laurence.gillian@towergate.co.uk>
2014-02-07 12:22:37 +0000

    Added messages and adjusted paramaterisation

--- PowerShell/Replace.ps1
+++ PowerShell/Replace.ps1
@@ -1,36 +1,63 @@
 # Parameters 
 Param (
-    [String]$List = "ReplacementList.csv",
-    [String]$Files = "*\*.*",
-    $COMPANY = "contoso",
-    $COMPANYFULLNAME = "Contoso Global Holdings 2014 Ltd.",
-    $PROJECT = "bank",
-    $debug=1
+    [Parameter(Mandatory=$true, HelpMessage="You must provide a company name.")]
+    $COMPANY,
+    [Parameter(Mandatory=$true, HelpMessage="You must provide a project name.")]
+    $PROJECT,
+    [Parameter(Mandatory=$true, HelpMessage="You must provide a company copyright notice. (e.g. Contoso Global Holdings Ltd, 2014.")]
+    $COMPANYFULLNAME,
+    $List = "ReplacementList.csv",
+    $Find = '*_company_._project_*' ,
+    $UpdateFiles = ('*.sln' , '*.csproj' , '*.cs' , '*.gitignore')
 )
 
-# Rename files and folders
-# Get-ChildItem -Filter "*_company._project_*" -Recurse | Rename-Item -NewName {$_.name -replace '_company_._project_',$COMPANY.$PROJECT }
+# Function for YES / NO logic
+Function Ask-YesOrNo
+{
+param([string]$title="Confirmation needed.",[string]$message="Parameters that will be used by the script are listed above.`nIf you want to modify one or more parameter, please restart the script and specify the wanted parameters.`nAny not defined parameter uses its default value.`r`nDo you want to continue with the above parameters?`n")
+$choiceYes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Answer Yes."
+$choiceNo = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Answer No."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($choiceYes, $choiceNo)
+$nresult = $host.ui.PromptForChoice($title, $message, $options, 1)
+switch ($nresult)
+    {
+        0 {"OK, we can continue..."}
+        1 {"Bye bye!" ; exit}
+    }
+}
 
-Write-Host "*** Renaming Files" -ForegroundColor Yellow
+# Welcome logic
+Write-Host "`n**********************************************************" -ForegroundColor Yellow
+Write-Host "*`t`tAutomatic Visual Studio Solution Creation" -ForegroundColor Yellow
+Write-Host "*" -ForegroundColor Yellow
+Write-Host "*" -ForegroundColor Yellow -nonewline; Write-Host " Parameters"
+Write-Host "*" -ForegroundColor Yellow -nonewline; Write-Host " Company Name (-COMPANY):`t`t" -nonewline; Write-Host "$COMPANY" -ForegroundColor DarkGreen
+Write-Host "*" -ForegroundColor Yellow -nonewline; Write-Host " Project Name (-PROJECT):`t`t" -nonewline; Write-Host "$PROJECT" -ForegroundColor DarkGreen
+Write-Host "*" -ForegroundColor Yellow -nonewline; Write-Host " Full Name (-COMPANYFULLNAME):`t`t" -nonewline; Write-Host "$COMPANYFULLNAME" -ForegroundColor DarkGreen
+Write-Host "*" -ForegroundColor Yellow
+Write-Host "*" -ForegroundColor Yellow -nonewline; Write-Host " Solution name: " -nonewline; Write-Host "$COMPANY.$PROJECT" -ForegroundColor DarkGreen
+Write-Host "*" -ForegroundColor Yellow
+Write-Host "**********************************************************" -ForegroundColor Yellow
+ 
+Ask-YesOrNo
+if ($nresult -eq "$false") {exit}
 
-$find = '*_company_._project_*'
+# Rename files, that match $Find
+Write-Host "Renaming matched files" -ForegroundColor Yellow
 
-foreach ($sc in Get-ChildItem -Filter $find -Recurse | where { test-path $_.fullname -pathtype leaf} ) {
+foreach ($sc in Get-ChildItem -Filter $Find -Recurse | where { test-path $_.fullname -pathtype leaf} ) {
 	
-	if ($debug) {
-		Write-Host "*** File found " -ForegroundColor Yellow
-		Get-ChildItem $sc.FullName
-	}
+    Write-Host "Found $sc" -ForegroundColor DarkGreen
 	
 	Get-ChildItem $sc.FullName | foreach { 
-		Write-Host $_ -ForegroundColor Yellow
 		rename-item $_ $_.Name.Replace('_company_._project_', "${COMPANY}.${PROJECT}") 
 	}
 
 }
 
 # Create an array that contains all the folders that should be renamed
-$folders = Get-ChildItem -Filter $find -recurse | Where-Object {$_.PSIsContainer -eq $True}  | Select-Object
+Write-Host "Finding folders" -ForegroundColor Yellow
+$folders = Get-ChildItem -Filter $Find -recurse | Where-Object {$_.PSIsContainer -eq $True}  | Select-Object
 
 # Reverse the array - we need to deal with the deepest items first!
 if ($folders) {
@@ -39,16 +66,12 @@ if ($folders) {
     foreach ($folder in $folders) {
         $folder.Name
         $folder.FullName
-
+        Write-Host "Renamed folder" -ForegroundColor DarkGreen
         Rename-Item $folder.FullName $folder.Name.Replace('_company_._project_', "${COMPANY}.${PROJECT}")
     } 
 }
 
-# Rename-Item $folder.FullName {$folder.Name -replace '_company_._project_', "${COMPANY}.${PROJECT}" }
-
-
-
-# Import Manual CSV into $ReplacementList variable
+# Import Manual CSV into $ReplacementList variable, this is a useful extension point
 $ReplacementList = Import-Csv $List;
 
 # Handle replacements from our this Powershell's params
@@ -68,16 +91,15 @@ $ReplacementList += $_Company, $_CompanyFullName, $_Project, $_GUID1, $_GUID2, $
 
 # Print out the replacement list
 if ($debug) {
-        Write-Host "*** Find and replace " -ForegroundColor Yellow
-        $ReplacementList
+    Write-Host "Running 'Find and replace'" -ForegroundColor Yellow
+    $ReplacementList
 }
 
 # Run through the files and update
-Get-ChildItem $Files |
+Get-ChildItem -Recurse -Include $UpdateFiles |
 ForEach-Object {
 
-    Write-Host "*** File found " -ForegroundColor Yellow
-
+    Write-Host "Updating - $_" -ForegroundColor DarkGreen
 
     $Content = Get-Content -Path $_.FullName;
     foreach ($ReplacementItem in $ReplacementList)
